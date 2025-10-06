d9842cfe73535e0796009aa012e4bce16e9fef1d
jorgegomezgerling <jorgegomezgerling@gmail.com>
2025-10-06 14:22:05 -0300

    [FIX chi2] cambios en dataset

--- clean_and_select.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sklearn.feature_selection import chi2
-
-raw_df = pd.read_csv('kaggle_dataset.csv')
-sintomas_df = raw_df.drop('prognosis', axis=1)
-enfermedades_df = raw_df['prognosis']
-
