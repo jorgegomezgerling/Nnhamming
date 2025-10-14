72888a82cfa1c90fb2188d7005876a345f968da6
jorgegomezgerling <jorgegomezgerling@gmail.com>
2025-10-14 14:44:20 -0300

    [DELETED] archivos innecesarios limpieza del proyecto

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from src.Nnhamming import *
+import random
+
+df = pd.read_csv('./dataset/bronze/kaggle_dataset.csv') 
+red = Nnhamming()
+red.fit_from_df(df)
+
+def cargar_vector_alteatorio(vector):
+    for i in range(0, 401):
+        vector_sintomas_aleatorios.append(random.randint(0,1))
+    return vector_sintomas_aleatorios
+
+def prueba_1(lista):
+    for indice, (etiqueta, nivel) in enumerate(lista):
+        print()
+        print(f'La enfermedad en la posición: {indice + 1} es: {etiqueta} con un nivel de coincidencia normalizado de: {nivel}')
+
+# Primeramente probamos con un vector de síntomas aleatorios:
+
+print("Enfermedades con carga de vector aleatorio:")
+
+vector_sintomas_aleatorios = []
+prueba_1(red.predict(cargar_vector_alteatorio(vector_sintomas_aleatorios), k=3))
+
+# Vector trastorno bipolar con un valor cambiado:
+
+print()
+print('*'*20)
+print()
+print('Enfermedades con carga de vector con 1 solo valor modificado: ')
+vector_bipolar_disorder = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+prueba_1(red.predict(vector_bipolar_disorder, k=3))
